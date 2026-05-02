34b843a570fad5521bacd77dda4009003f8ab91d
hanch2396 <hanch2396@gmail.com>
2026-05-02 18:20:17 +0900

    Revert "`podman`/`docker` 백엔드를 직접 사용하지 않고 `distrobox`를 사용하도록 변경"
    This reverts commit 0c160acc73452b5b972d516e9923695e4127a649.

--- scripts/linux/build-image.sh
+++ scripts/linux/build-image.sh
@@ -5,20 +5,22 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/config-container.sh"
 
-echo "--- [$DOCKER_CMD]를 사용하여 이미지를 빌드합니다: $IMAGE_NAME:$IMAGE_TAG"
+echo "--- [$DOCKER_CMD]를 사용하여 이미지를 빌드합니다: $IMAGE_NAME:$IMAGE_TAG ---"
 
 $DOCKER_CMD rm -f $CONTAINER_NAME 2>/dev/null || true
 # $DOCKER_CMD rmi -f $IMAGE_NAME:$IMAGE_TAG || true
 
 # 빌드 수행
+# --no-cache 옵션은 필요할 때만 추가하세요.
 $DOCKER_CMD build \
     --pull=newer \
+    --build-arg USER_NAME=$USER_NAME \
     -t "$IMAGE_NAME:$IMAGE_TAG" $PROJECT_DIR
 
 # 결과 확인
 if [ $? -eq 0 ]; then
-    echo "--- 빌드 성공!"
+    echo "--- 빌드 성공! ---"
 else
-    echo "--- 빌드 실패 :("
+    echo "--- 빌드 실패 ---"
     exit 1
 fi
