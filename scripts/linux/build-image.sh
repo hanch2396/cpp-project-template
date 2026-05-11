82e3ebd1b7adc145524c88a64603098e306e64ec
hanch2396 <hanch2396@gmail.com>
2026-05-11 22:38:56 +0900

    이미지 빌드 옵션을 인자로 받을 수 있도록 수정

--- scripts/linux/build-image.sh
+++ scripts/linux/build-image.sh
@@ -12,6 +12,7 @@ echo "--- [$DOCKER_CMD]를 사용하여 이미지를 빌드합니다: $IMAGE_NAM
 # 빌드 수행
 # --no-cache 옵션은 필요할 때만 추가하세요.
 $DOCKER_CMD build \
+    "$@" \
     --pull=newer \
     --build-arg USER_NAME=$USER_NAME \
     -t "$IMAGE_NAME:$IMAGE_TAG" $PROJECT_DIR
